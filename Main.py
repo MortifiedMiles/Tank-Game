9817545640db8e0da4600a56d87bad1237f3d5bc
Miles <78830914+MortifiedMiles@users.noreply.github.com>
2025-05-23 21:41:33 -0600

    Creating the initial window, and starting the tank class

--- Main.py
+++ Main.py
@@ -1,1 +1,31 @@
+import pygame
+import sys
+from pygame import *
 
+# Define Constants
+black = (0, 0, 0)
+window_width = 600
+window_height = 600
+frames_per_second = 30
+
+# initialize world
+pygame.init()
+window = pygame.display.set_mode((window_width,window_height))
+clock = pygame.time.Clock()
+
+# initialize loop
+while True:
+
+    # Event checker
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.quit()
+    
+
+
+    window.fill(black)
+
+    pygame.display.update
+
+    clock.tick(frames_per_second)
